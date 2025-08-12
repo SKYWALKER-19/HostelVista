dc2f6a93fb7bd75c600ee08ffedc4e13b95771c5
Ayush Tripathi <ayustripathi19@gmail.com>
2025-08-12 22:42:48 +0530

    Added CORS support to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import './models/db.js'
 import router from './routes/AuthRouter.js'
+import cors from "cors"
 import dotenv from 'dotenv';
 dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 app.use(express.json());
 
 app.use('/api',router);
